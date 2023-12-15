45bbdac80af0f58b943eff1638d93f84c6bd6af5
Ángel <angeldiazcalleja@gmail.com>
2023-12-15 17:42:09 +0100

    feat: Initialice repository

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+// import { Header } from './common/Header/Header'
+import { Router } from './pages/Routes/Router';
+import smoothscroll from 'smoothscroll-polyfill';
+ 
+smoothscroll.polyfill();
+
+function App() {
+
+  return (
+    <>
+      {/* <Header/> */}
+      <Router/>
+    </>
+  )
+}
+
+export default App
+
+
+
